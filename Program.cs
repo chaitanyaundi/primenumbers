2fb0990fb695351880373f9d7d6f7029bbda7f0c
chaitanya.u <chaitanya.u@avrhil.in>
2022-01-04 00:09:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace primenumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var box = new List<int>();
+            Console.Write("Enter the number for the range of primes till this number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 1; j < num; j++)
+            {
+                if (Factors(j) == 2)
+                {
+                    box.Add(j);
+                }
+            }
+            foreach (var item in box)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total number of primes till {num} are {box.Count}.");
+        }
+
+        public static int Factors(int x)
+        {
+            var box2 = new List<int>();
+            for (int i = 1; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    box2.Add(i);
+                }
+            }
+            return box2.Count;
+        }
+
+
+    }
+}
